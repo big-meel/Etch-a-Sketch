1a86f38ab8bd8fdd4e3f52d3c990a7dcc304797d
Jamz <big.meel@zoho.com>
2019-08-01 00:10:28 -0500

    Set to run multiple functions on load

--- sketch-script.js
+++ sketch-script.js
@@ -1,17 +1,20 @@
 const container = document.querySelector('#container');
 container.setAttribute('style', 'display: grid; grid-template-columns: repeat(16, auto);\
- border: 2px solid blue; ');
+ border: 5px solid orange; grid-template-rows: repeat(16, auto)');
+
 
 
 function createGrid(){
     for (i = 0; i < (16*16); i++){
         const square = document.createElement('div');
         square.classList.add('grid-square');
-        square.textContent = i;
-        square.setAttribute('style', 'background-color: skyblue; padding: 20px;\
-         text-align: center; border: 1px solid lightgrey');
+        square.setAttribute('style', 'background-color: lightgrey; padding: 18px;\
+        text-align: center; border: 0.5px solid black');
         container.appendChild(square);
     }       
 }
 
-window.addEventListener('load', createGrid());
+
+window.addEventListener('load', () => {
+    createGrid();
+});
